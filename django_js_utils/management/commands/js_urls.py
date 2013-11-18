f13e0e1375bde1b223bf6be782c4373c90422816
Matt Keeble <matt.keeble@gmail.com>
2013-11-18 16:56:54 +0000

    ensuring user can override output location

--- django_js_utils/management/commands/js_urls.py
+++ django_js_utils/management/commands/js_urls.py
@@ -20,7 +20,14 @@ class Command(BaseCommand):
         Create urls.js file by parsing all of the urlpatterns in the root urls.py file
         """
         js_patterns = SortedDict()
-        print "Generating Javascript urls file %s" % app_settings.URLS_JS_GENERATED_FILE
+
+        outputLoc = ''
+        if not project_settings.URLS_JS_GENERATED_FILE:
+            outputLoc = app_settings.URLS_JS_GENERATED_FILE
+        else:
+            outputLoc = project_settings.URLS_JS_GENERATED_FILE
+
+        print "Generating Javascript urls file %s" % outputLoc
         Command.handle_url_module(js_patterns, project_settings.ROOT_URLCONF)
         #output to the file
         urls_file = open(app_settings.URLS_JS_GENERATED_FILE, "w")
