bcc9ef9cebbd1004af0fcccef88c0f55754e092c
Philippe May <philippe.may@hanjinet.org>
2013-12-11 18:59:49 +0530

    Fix missing prefix when the pattern is a RegexURLResolver

--- django_js_utils/management/commands/js_urls.py
+++ django_js_utils/management/commands/js_urls.py
@@ -12,7 +12,7 @@ from django.conf import settings
 
 RE_KWARG = re.compile(r"(\(\?P\<(.*?)\>.*?\))") #Pattern for recongnizing named parameters in urls
 RE_ARG = re.compile(r"(\(.*?\))") #Pattern for recognizing unnamed url parameters
-    
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -34,7 +34,7 @@ class Command(BaseCommand):
         urls_file.write(";")
         urls_file.close()
         print "Done generating Javascript urls file %s" % URLS_JS_GENERATED_FILE
-    
+
     @staticmethod
     def handle_url_module(js_patterns, module_name, prefix=""):
         """
@@ -43,39 +43,44 @@ class Command(BaseCommand):
         """
 
         if isinstance(module_name, basestring):
+            print('%s: %s' % (module_name, prefix))
             __import__(module_name)
             root_urls = sys.modules[module_name]
             patterns = root_urls.urlpatterns
         elif isinstance(module_name, object):
             if hasattr(module_name, 'urlpatterns'):
+                print('module %s: %s' % (module_name.__name__, prefix))
                 patterns = module_name.urlpatterns
 
         if not 'patterns' in locals():
-            root_urls = module_name
-            patterns = root_urls
+            patterns = module_name
 
         try:
             for pattern in patterns:
                 if issubclass(pattern.__class__, RegexURLPattern):
-                    if pattern.name:
-                        full_url = prefix + pattern.regex.pattern
-                        for chr in ["^","$"]:
-                            full_url = full_url.replace(chr, "")
-                        #handle kwargs, args
-                        kwarg_matches = RE_KWARG.findall(full_url)
-                        if kwarg_matches:
-                            for el in kwarg_matches:
-                                #prepare the output for JS resolver
-                                full_url = full_url.replace(el[0], "<%s>" % el[1])
-                        #after processing all kwargs try args
-                        args_matches = RE_ARG.findall(full_url)
-                        if args_matches:
-                            for el in args_matches:
-                                full_url = full_url.replace(el, "<>")#replace by a empty parameter name
-                        js_patterns[pattern.name] = "/" + full_url
+                    if not pattern.name:
+                        continue
+                    full_url = prefix + pattern.regex.pattern
+                    for chr in ["^","$"]:
+                        full_url = full_url.replace(chr, "")
+                    # Handle kwargs, args
+                    kwarg_matches = RE_KWARG.findall(full_url)
+                    if kwarg_matches:
+                        for el in kwarg_matches:
+                            ## Prepare the output for JS resolver
+                            full_url = full_url.replace(el[0], "<%s>" % el[1])
+                    #after processing all kwargs try args
+                    args_matches = RE_ARG.findall(full_url)
+                    if args_matches:
+                        for el in args_matches:
+                            ## Replace by a empty parameter name
+                            full_url = full_url.replace(el, "<>")
+                    js_patterns[pattern.name] = "/" + full_url
                 elif issubclass(pattern.__class__, RegexURLResolver):
-                    if pattern.urlconf_name:
-                        Command.handle_url_module(js_patterns, pattern.urlconf_name, prefix=pattern.regex.pattern)
+                    if not pattern.urlconf_name:
+                        continue
+                    Command.handle_url_module(js_patterns, pattern.urlconf_name,
+                                              prefix=prefix + pattern.regex.pattern)
         except TypeError:
             print "couldn't iterate over patterns"
             print patterns
